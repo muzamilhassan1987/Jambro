f274d340e193635532d25aef7dedb4f3cc0a3794
farazhaider <farazhaider88@yahoo.com>
2016-10-14 17:05:09 +0500

    Initial Commit

---
+++ Jambro/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Jambro
+//
+//  Created by Faraz Haider on 14/10/2016.
+//  Copyright © 2016 Faraz Haider. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Jambro/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Jambro
+//
+//  Created by Faraz Haider on 14/10/2016.
+//  Copyright © 2016 Faraz Haider. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
